de6cc0f18ee0f311940f479db4a0ef6b8d0646a3
Mark Blows <mblows@gmail.com>
2015-08-07 15:15:11 +1000

    Update exercise_01.py

--- 2015-08-07/solutions/exercise_01.py
+++ 2015-08-07/solutions/exercise_01.py
@@ -1,1 +1,16 @@
 
+#!/usr/bin/env python3
+# coding=utf-8
+
+def is_prime(n):
+    for x in range(2, n):
+        if n % x == 0:
+            return False
+
+    return True
+
+n = int(input("Please enter the upper range: "))
+
+for n in range(1, n + 1):
+    if is_prime(n):
+        print(n)
