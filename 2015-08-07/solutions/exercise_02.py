dd615a62f763c9b148adc5e94e2d2b951761e1da
Mark Blows <mblows@gmail.com>
2015-08-07 15:21:18 +1000

    Create exercise_02.py

---
+++ 2015-08-07/solutions/exercise_02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+sentence = input("Please enter a sentence: ")
+
+vowels = 0
+
+for char in sentence:
+    if char in "aeiou":
+        vowels += 1
+
+print("There are {} vowels and {} consonants in your sentence.".format(
+    vowels, len(sentence) - vowels))
