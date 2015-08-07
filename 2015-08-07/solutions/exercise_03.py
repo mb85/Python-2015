c15c3a8a35af97bde29b75f2626de58db8232fda
Mark Blows <mblows@gmail.com>
2015-08-07 15:21:50 +1000

    Create exercise_03.py

---
+++ 2015-08-07/solutions/exercise_03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from random import randint
+
+n_rolls = input("How many rolls would you like? ")
+
+for n in range(int(n_rolls)):
+    a, b, c = (randint(1, 6), randint(1, 6), randint(1, 6))
+    print("Roll {}: {}, {}, {}".format(n + 1, a, b, c))
