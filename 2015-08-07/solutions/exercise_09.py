7323842baddf198c50564b95727f71fa9598d359
Mark Blows <mblows@gmail.com>
2015-08-14 11:14:12 +1000

    Update exercise_09.py

--- 2015-08-07/solutions/exercise_09.py
+++ 2015-08-07/solutions/exercise_09.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+n_words = 0
+
+with open("Alice's Adventures in Wonderland.txt") as open_file:
+    for line in open_file:
+        n_words += len(line.split())
+
+print(n_words)
