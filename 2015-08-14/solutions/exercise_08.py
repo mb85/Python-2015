2df01fc02a37f9cd0f78a414b68bc8f2044a1b0f
Mark Blows <mblows@gmail.com>
2015-08-28 14:52:26 +1000

    Updated new exercises

--- 2015-08-14/solutions/exercise_08.py
+++ 2015-08-14/solutions/exercise_08.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+    if args:
+        word = args[0]
+        if len(set(word)) == len(word):
+            print("All letters in this word are unique.")
+        else:
+            print("Not all letters in this word are unique.")
+
+if __name__ == "__main__":
+    sys.exit(main())
